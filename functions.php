92aa70a59bc4d087a953c169d90cf1b31e3ffe0e
fmollaei7-debug <fmollaei7@gmail.com>
2026-09-22 05:50:57 +0330

    Bump premium checkout assets to 1.0.31

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /* -------------------------------------------------------------------------
  * ثابت‌های قالب
  * ---------------------------------------------------------------------- */
-define( 'BAJISTYLE_VERSION', '1.0.30' );
+define( 'BAJISTYLE_VERSION', '1.0.31' );
 define( 'BAJISTYLE_DIR', get_template_directory() );
 define( 'BAJISTYLE_URI', get_template_directory_uri() );
 
