66f5aa053758278b2739f70554da4b9de9193738
fmollaei7-debug <fmollaei7@gmail.com>
2026-09-23 01:00:05 +0330

    Bump BAJI Size Studio assets to 1.0.41

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /* -------------------------------------------------------------------------
  * ثابت‌های قالب
  * ---------------------------------------------------------------------- */
-define( 'BAJISTYLE_VERSION', '1.0.40' );
+define( 'BAJISTYLE_VERSION', '1.0.41' );
 define( 'BAJISTYLE_DIR', get_template_directory() );
 define( 'BAJISTYLE_URI', get_template_directory_uri() );
 
