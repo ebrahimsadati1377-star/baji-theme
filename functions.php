10c458aa81274d442f8526e2e2eb5a4e4a362a8b
fmollaei7-debug <fmollaei7@gmail.com>
2026-09-23 05:53:57 +0330

    Load standalone homepage hero slider and bump assets

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /* -------------------------------------------------------------------------
  * ثابت‌های قالب
  * ---------------------------------------------------------------------- */
-define( 'BAJISTYLE_VERSION', '1.0.49' );
+define( 'BAJISTYLE_VERSION', '1.0.50' );
 define( 'BAJISTYLE_DIR', get_template_directory() );
 define( 'BAJISTYLE_URI', get_template_directory_uri() );
 
@@ -346,6 +346,17 @@ function bajistyle_enqueue_assets() {
 				'bajistyle-product-stories',
 				BAJISTYLE_URI . '/assets/js/product-stories.js',
 				array(),
+				BAJISTYLE_VERSION,
+				array(
+					'in_footer' => true,
+					'strategy'  => 'defer',
+				)
+			);
+
+			wp_enqueue_script(
+				'bajistyle-hero-slider',
+				BAJISTYLE_URI . '/assets/js/hero-slider.js',
+				array( 'swiper-js' ),
 				BAJISTYLE_VERSION,
 				array(
 					'in_footer' => true,
