dd6403cef967fb4e96be921064712e0299fa438e
fmollaei7-debug <fmollaei7@gmail.com>
2026-09-23 06:27:43 +0330

    Bump hero slider assets to v1.0.54

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /* -------------------------------------------------------------------------
  * ثابت‌های قالب
  * ---------------------------------------------------------------------- */
-define( 'BAJISTYLE_VERSION', '1.0.53' );
+define( 'BAJISTYLE_VERSION', '1.0.54' );
 define( 'BAJISTYLE_DIR', get_template_directory() );
 define( 'BAJISTYLE_URI', get_template_directory_uri() );
 
