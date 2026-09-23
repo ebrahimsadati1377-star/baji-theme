0253489c75b207824162e0f8976e2c5064e88b0c
fmollaei7-debug <fmollaei7@gmail.com>
2026-09-23 05:37:03 +0330

    Bump shop styles after live compatibility fix

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /* -------------------------------------------------------------------------
  * ثابت‌های قالب
  * ---------------------------------------------------------------------- */
-define( 'BAJISTYLE_VERSION', '1.0.46' );
+define( 'BAJISTYLE_VERSION', '1.0.47' );
 define( 'BAJISTYLE_DIR', get_template_directory() );
 define( 'BAJISTYLE_URI', get_template_directory_uri() );
 
